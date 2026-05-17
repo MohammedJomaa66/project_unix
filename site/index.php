7436036e6f15fa44cb4beb6e25bf65a5c80d8f41
mj <s12112655@stu.najah.edu>
2026-05-17 21:59:35 +0300

    6th

--- site/index.php
+++ site/index.php
@@ -8,7 +8,7 @@ $dbname = "dict";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
-    die("DB Connection failed: " . $conn->connect_error);
+    die("DB Connection failed2: " . $conn->connect_error);
 }
 echo "DB Connected Successfully";
 
